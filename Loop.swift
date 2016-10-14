a58c755759e8d6e2a43b580aeac12fc6b087b57c
karavolt <zpdlxkfh@gmail.com>
2016-10-14 11:16:16 +0900

    Update Loop.swift

--- Loop.swift
+++ Loop.swift
@@ -8,23 +8,31 @@ for area in areas {
 let Levels = [98, 100,102,104,105,106,108,110]
 for level in Levels {
     // 홀수 숫자만 프린트 하세요
-    print (level)
+    if level % 2 == 1 {
+        print (level)
+    }
 }
 
 let Races = ["나이트엘프", "블러드엘프", "오크"]
+var cnt:Int = 0
 for race in Races {
     // 종족명 뒤에 루프순서대로 번호를 추가해서 프린트하세요
-    print(race)
+    cnt += 1
+    print(race + "\(cnt)")
 }
 
 let ItemLevels = [740,800,820,810,835,755,775,845,795,817,873]
+var sum:Int = 0
 for avg in ItemLevels{
     // 아이템 레벨의 평균을 구하세요
-    print(avg)
+    sum = sum + avg
+    print(sum/11)
 }
 
 let Damage = [-10000, 2500, -4000, -2000, -1000, 30000, -25000, -5000]
-for sum in Damage {
+sum = 0
+for hit in Damage {
     // 총합이 0 이상이면 생존 0이하면 죽음으로 표시하세요
-    print(sum)
+    sum = sum + hit
+    print(hit)
 }
